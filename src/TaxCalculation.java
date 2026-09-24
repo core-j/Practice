3d8696c1e6947990ee3373522739b92491755660
Chaithanya <chaithanya261998@gmail.com>
2026-09-24 22:46:54 +0530

    Initial commit

---
+++ src/TaxCalculation.java
@@ -0,0 +1,7 @@
+public class TaxCalculation {
+    public static void main(String[] args) {
+        int salary= 150000;
+        int taxAmount=(10*salary)/100;
+            System.out.println("taxAmount is: "+taxAmount);
+        }
+}
